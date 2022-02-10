99977cdf866b0f226e5cc3a49b38fda6a96982da
Brendon J. Brewer <brendon.brewer@gmail.com>
2022-02-11 12:03:08 +1300

    canonical model

--- R/ns-model.R
+++ R/ns-model.R
@@ -1,9 +1,11 @@
 # Define the number of parameters
-num_params = 3
+num_params = 7
 
 # Define the names of the parameters.
 # If you can't be bothered, use parameter_names=rep(NA, num_params).
-parameter_names = c("A", "phi", "dispersion")
+parameter_names = c("A1", "phi1", "dispersion1",
+                    "A2", "phi2", "dispersion2",
+                    "z_crit")
 
 # A dataset
 # This could easily be loaded from an external file
@@ -26,9 +28,13 @@ us_to_params = function(us)
 
     #### You'll only need to edit this function below this line ####
 
-    params["A"]   = qunif(us[1], 0,  200)
-    params["phi"] = qunif(us[2], -pi, pi)
-    params["dispersion"] = qunif(us[3], 0, 400)
+    params["A1"]   = qunif(us[1], 0,  800)
+    params["phi1"] = qunif(us[2], -pi, pi)
+    params["dispersion1"] = qunif(us[3], 0, 400)
+    params["A2"]   = qunif(us[4], 0,  800)
+    params["phi2"] = qunif(us[5], -pi, pi)
+    params["dispersion2"] = qunif(us[6], 0, 400)
+    params["z_crit"] = qunif(us[7], -3, -1)
 
     return(params)
 }
@@ -37,8 +43,12 @@ us_to_params = function(us)
 # log likelihood.
 log_likelihood = function(params)
 {
-    mu = params["A"]*sin(data$theta - params["phi"])
-    sig = sqrt(params["dispersion"]^2 + data$sig_v^2)
+    mu = params["A1"]*sin(data$theta - params["phi1"])
+    greater = data$z > params["z_crit"]
+    mu[greater] = params["A2"]*sin(data$theta[greater] - params["phi2"])
+
+    sig = sqrt(params["dispersion1"]^2 + data$sig_v^2)
+    sig[greater] = sqrt(params["dispersion2"]^2 + data$sig_v[greater]^2)
     sum(dnorm(data$v, mu, sig, log=TRUE))
 }
 
