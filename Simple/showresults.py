f15e0ee804d009a5514e07128c5964c1cba0d332
Brendon J. Brewer <brendon.brewer@gmail.com>
2022-02-11 12:58:10 +1300

    Plot angles

--- Simple/showresults.py
+++ Simple/showresults.py
@@ -11,8 +11,34 @@ if __name__ == "__main__":
     import showresults as sr
     sr.standard_results(sys.argv)
 
+    import numpy as np
     import pandas as pd
-    output = pd.read_csv("output/posterior.csv")
+    samples = pd.read_csv("output/posterior.csv")
     import corner
-    corner.corner(output, labels=output.columns)
+    corner.corner(samples, labels=samples.columns)
     plt.show()
+
+
+    data = np.loadtxt("../DNest5/data.txt")
+    colors = ["red" if v < 0.0 else "blue" for v in data[:,4]]
+    plt.scatter(data[:,0], data[:,1], s=0.1*np.abs(data[:,4]), marker="o",
+                alpha=0.3, color=colors)
+    plt.xlim([-5, 5])
+    plt.ylim([-5, 5])
+    plt.axis("square")
+
+    phi1 = np.array(samples["phi1"])
+    phi2 = np.array(samples["phi2"])
+#    phi1 = phi1[samples["z_crit"] < -1.5]
+#    phi2 = phi2[samples["z_crit"] < -1.5]
+
+    xs = np.linspace(-5, 5, 11)
+    for i in range(min(1000, len(phi1))):
+        ys = np.tan(phi1[i])*xs
+        plt.plot(xs, ys, "m-", linewidth=1, alpha=0.01)
+        ys = np.tan(phi2[i])*xs
+        plt.plot(xs, ys, "c-", linewidth=1, alpha=0.01)
+
+
+    plt.show()
+
