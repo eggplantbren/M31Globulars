ec7354db95c9e2f66f3c7ea4cd23997976e2479d
Brendon J. Brewer <brendon.brewer@gmail.com>
2025-05-31 08:45:18 +1200

    Make a corner plot

--- New/showresults.py
+++ New/showresults.py
@@ -1,3 +1,20 @@
 import dnest4.classic as dn4
+import corner
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+
 dn4.postprocess()
+
+os.system("R CMD BATCH convert.R")
+
+
+posterior_samples = pd.read_csv("postdf.csv")
+corner.corner(posterior_samples,
+              plot_density=False, plot_contours=False, fontsize=14,
+              hist_kwargs={"color":"blue", "alpha":0.3, "histtype":"stepfilled",
+                           "edgecolor":"black","lw":"3"})
+
+plt.show()
 
